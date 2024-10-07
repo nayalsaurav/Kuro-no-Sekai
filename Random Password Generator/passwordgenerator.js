680e9179926949069318d25a1c4a9190586ac2b0
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-10-07 16:38:56 +0000

    slider

---
+++ Random Password Generator/passwordgenerator.js
@@ -0,0 +1,15 @@
+
+const length = document.getElementById('length');
+const rangeValue = document.getElementById('range-value');
+const number = document.getElementById('number');
+const lowerCase = document.getElementById('lowercase-character');
+const upperCase = document.getElementById('uppercase-character');
+const specialChar = document.getElementById('special-character');
+
+length.addEventListener('input',()=>{
+    rangeValue.textContent=length.value;
+});
+
+function isSelected(option) {
+    option.
+}
