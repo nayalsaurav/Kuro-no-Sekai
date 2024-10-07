4cbcca97c70b3cb2ad701e82ef037177627b6694
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-10-07 18:19:42 +0000

    added logic

--- Random Password Generator/passwordgenerator.js
+++ Random Password Generator/passwordgenerator.js
@@ -1,15 +1,41 @@
 
-const length = document.getElementById('length');
+const paswordLength = document.getElementById('length');
 const rangeValue = document.getElementById('range-value');
 const number = document.getElementById('number');
 const lowerCase = document.getElementById('lowercase-character');
 const upperCase = document.getElementById('uppercase-character');
 const specialChar = document.getElementById('special-character');
+const generatebtn = document.getElementById('generatebtn');
+const passValue = document.getElementById('pasword-value');
 
-length.addEventListener('input',()=>{
-    rangeValue.textContent=length.value;
+paswordLength.addEventListener('input',()=>{
+    rangeValue.textContent=paswordLength.value;
 });
 
-// function isSelected(option) {
-//     option.
-// }
+
+generatebtn.addEventListener('click',()=>{
+    passValue.value = generatePassword();
+});
+
+let uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercaseString = "abcdefghijklmnopqrstuvwxyz";
+let specialCharString = "~!@#$%^&*";
+let numberString = "0123456789"; 
+
+function generatePassword() {
+    let selectedStrings = "";
+    let currentPassword ="";
+    selectedStrings+= upperCase.checked ? uppercaseString:"";
+    selectedStrings+= lowerCase.checked ? lowercaseString:"";
+    selectedStrings+= number.checked ? numberString:"";
+    selectedStrings+= specialChar.checked ? specialCharString:"";
+    let currSize = paswordLength.value;
+    for(let i=0;i<currSize;i++){
+        currentPassword += selectedStrings.charAt(Math.floor(Math.random()*selectedStrings.length));
+    }
+    return currentPassword;
+}
+
+copyIcon.addEventListener('click',()=>{
+    navigator.clipboard.write(passValue);
+});
