cb0f2ba8e87335902849c237299222b01e364ce8
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-10-08 16:08:45 +0000

    added css

--- Random Password Generator/passwordgenerator.js
+++ Random Password Generator/passwordgenerator.js
@@ -7,35 +7,42 @@ const upperCase = document.getElementById('uppercase-character');
 const specialChar = document.getElementById('special-character');
 const generatebtn = document.getElementById('generatebtn');
 const passValue = document.getElementById('pasword-value');
-
-paswordLength.addEventListener('input',()=>{
-    rangeValue.textContent=paswordLength.value;
+const copyIcon = document.getElementById('copybtn');
+const copied = document.querySelector('.copied');
+paswordLength.addEventListener('input', () => {
+    rangeValue.textContent = paswordLength.value;
 });
 
 
-generatebtn.addEventListener('click',()=>{
+generatebtn.addEventListener('click', () => {
     passValue.value = generatePassword();
 });
 
 let uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowercaseString = "abcdefghijklmnopqrstuvwxyz";
 let specialCharString = "~!@#$%^&*";
-let numberString = "0123456789"; 
+let numberString = "0123456789";
 
 function generatePassword() {
     let selectedStrings = "";
-    let currentPassword ="";
-    selectedStrings+= upperCase.checked ? uppercaseString:"";
-    selectedStrings+= lowerCase.checked ? lowercaseString:"";
-    selectedStrings+= number.checked ? numberString:"";
-    selectedStrings+= specialChar.checked ? specialCharString:"";
+    let currentPassword = "";
+    selectedStrings += upperCase.checked ? uppercaseString : "";
+    selectedStrings += lowerCase.checked ? lowercaseString : "";
+    selectedStrings += number.checked ? numberString : "";
+    selectedStrings += specialChar.checked ? specialCharString : "";
     let currSize = paswordLength.value;
-    for(let i=0;i<currSize;i++){
-        currentPassword += selectedStrings.charAt(Math.floor(Math.random()*selectedStrings.length));
+    for (let i = 0; i < currSize; i++) {
+        currentPassword += selectedStrings.charAt(Math.floor(Math.random() * selectedStrings.length));
     }
     return currentPassword;
 }
 
-copyIcon.addEventListener('click',()=>{
-    navigator.clipboard.write(passValue);
+copyIcon.addEventListener('click', () => {
+    if(passValue.value!=""){
+    navigator.clipboard.writeText(passValue.value);
+    copied.style.display = 'block';
+    setTimeout(() => {
+        copied.style.display = 'none';
+    }, 700);}
 });
+
